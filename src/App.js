b4f7ed2b8339d3fd7caece984830fbb79c6fe8a4
Nachoxt17 <nacho.man.kpo@hotmail.com>
2020-09-22 16:15:51 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FetchCard from './FetchCard';
+import Lifecycle from './Lifecycle';
+import ResizeApp from './ResizeApp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Lifecycle />
+            <FetchCard />
+            <ResizeApp />
+        </div>
+    );
 }
 
 export default App;
